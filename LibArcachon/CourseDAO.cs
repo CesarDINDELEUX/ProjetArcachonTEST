d9afd5d34289e20749a4d37c3e98726343653293
Alexis <alexis.moriceau@epsi.fr>
2017-04-06 13:53:16 +0200

    sdv56

--- LibArcachon/CourseDAO.cs
+++ LibArcachon/CourseDAO.cs
@@ -9,7 +9,7 @@ namespace LibArcachon
     public class CourseDAO
     {
         /// <summary>
-        /// Liste Toutesss les courszdqzde et renvois une liste de courses
+        /// Liste Toutessees les courszdqzde et renvois une liste de courses
         /// </summary>
         /// <returns>ListeCourse comme une List Course</returns>
         public static List<Course> List() {
