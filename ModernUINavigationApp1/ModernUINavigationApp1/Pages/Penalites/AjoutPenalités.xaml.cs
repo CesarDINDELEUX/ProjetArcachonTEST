fe30ad96533128e4504e61f0cba107139d9136ed
Alexis <alexis.moriceau@epsi.fr>
2017-04-10 14:49:05 +0200

    ajout de pages

---
+++ ModernUINavigationApp1/ModernUINavigationApp1/Pages/Penalites/AjoutPenalités.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernUINavigationApp1.Pages.Penalites
+{
+    /// <summary>
+    /// Interaction logic for AjoutPenalités.xaml
+    /// </summary>
+    public partial class AjoutPenalités : UserControl
+    {
+        public AjoutPenalités()
+        {
+            InitializeComponent();
+        }
+    }
+}
